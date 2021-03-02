51064afd0366ea2dd1fd78e1e1d86647d99a15da
IrvingMera <marcos.mera.contractor@bbva.com>
2021-03-02 16:05:25 -0500

    v2 irving

---
+++ src/pages/tipologias.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FrameGraphic from "../components/frame-graphic";
+
+const Tipologias = () => {
+  return (
+    <div className="Home PageView">
+      <h1 className="MainContent__title">Tipologías</h1>
+      <div className="Graphic">
+        <FrameGraphic source="https://datastudio.google.com/reporting/1mhBeJoVZvwRqKieAXvb2GcoQCY3AMFX4/page/bo6RB" />
+      </div>
+    </div>
+  );
+};
+
+export default Tipologias;
